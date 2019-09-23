8cfcb120f499bb03cc94fe5fda7613fa9ebe33a0
Steve Howe <Stevehowe10@gmail.com>
2019-09-23 10:39:19 -0600

    initial html and ajax call

---
+++ assets/app.js
@@ -0,0 +1,33 @@
+
+$("#submitButton").on("click", function(event){
+event.preventDefault();
+
+    var settings = {
+        "async": true,
+	"crossDomain": true,
+	"url": "https://api.meaningcloud.com/summarization-1.0",
+	"method": "POST",
+	"headers": {
+        "content-type": "application/x-www-form-urlencoded"
+    },
+    "data": {
+        "key": "d786ff4b264451fd2de354055e04f66a",
+        "txt": "",
+        "url": $("#url-input").val().trim(),
+        "doc": "",
+        "sentences": $("#sentences-input").val().trim(),
+    }
+}
+
+// var sent = $("#sentences-input").va;().trim
+// var url = $("#url-input").val().trim();
+
+
+// console.log(txt)
+// console.log(url)
+$.ajax(settings).done(function (response) {
+    console.log(response);
+    
+    
+});
+})
