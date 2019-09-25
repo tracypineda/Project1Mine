b511bd29c61a04f61d73c11ebca1aa48b7e66da7
Nick Cooper <charliecoop@gmail.com>
2019-09-25 11:38:53 -0600

    simplified our javascript

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,98 +1,75 @@
-
-$("#submitButton").on("click", function(event){
-event.preventDefault();
-
-
-    if ($("#search-input").val() === ""){
-        var settings = {
-            "async": true,
-        "crossDomain": true,
-        "url": "https://api.meaningcloud.com/summarization-1.0",
-        "method": "POST",
-        "headers": {
-            "content-type": "application/x-www-form-urlencoded"
-        },
-        "data": {
-            "key": "d786ff4b264451fd2de354055e04f66a",
-            "txt": $("#text-input").val().trim(),
-            "url": $("#url-input").val().trim(),
-            "doc": "",
-            "sentences": $("#sentences-input").val().trim(),
-        }
-}
-} else {
-    var settings = {
-        "async": true,
-	"crossDomain": true,
-	"url": "https://api.meaningcloud.com/summarization-1.0",
-	"method": "POST",
-	"headers": {
+// Settings for summarization api
+var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://api.meaningcloud.com/summarization-1.0",
+    "method": "POST",
+    "headers": {
         "content-type": "application/x-www-form-urlencoded"
     },
     "data": {
         "key": "d786ff4b264451fd2de354055e04f66a",
-        "txt": $("#text-input").val().trim(),
-        "url": "https://en.wikipedia.org/wiki/" + $("#search-input").val().trim(),
+        "txt": "",
+        "url": "",
         "doc": "",
-        "sentences": $("#sentences-input").val().trim(),
+        "sentences": "",
     }
 }
-}
 
-$.ajax(settings).done(function (response) {
-    $("#text-output").html(response.summary)
-    
-    
-});
-});
-
-$("#random").on("click", function(event){
+// Submit button on click event
+$("#submitButton").on("click", function (event) {
     event.preventDefault();
-    
-        var settings = {
-            "async": true,
-        "crossDomain": true,
-        "url": "https://api.meaningcloud.com/summarization-1.0",
-        "method": "POST",
-        "headers": {
-            "content-type": "application/x-www-form-urlencoded"
-        },
-        "data": {
-            "key": "d786ff4b264451fd2de354055e04f66a",
-            "txt": "",
-            "url": "https://en.wikipedia.org/wiki/Special:Random",
-            "doc": "",
-            "sentences": $("#sentences-input").val().trim(),
-        }
-    };
-    $.ajax(settings).done(function (response) {        
-        $("#text-output").html(response.summary)
-        
-        
+
+    if ($("#search-input").val() === "") {
+        settings.data.txt = $("#text-input").val().trim();
+        settings.data.url = $("#url-input").val().trim();
+        settings.data.sentences = $("#sentences-input").val().trim();
+    } else {
+        settings.data.txt = $("#text-input").val().trim();
+        settings.data.url = "https://en.wikipedia.org/wiki/" + $("#search-input").val().trim();
+        settings.data.sentences = $("#sentences-input").val().trim();
+    }
+
+    $.ajax(settings).done(function (response) {
+        $("#text-output").html(response.summary);
     });
 });
 
-$("#speechButton").on("click", function(event) {
+// Random button on click event
+$("#random").on("click", function (event) {
     event.preventDefault();
 
+    settings.data.txt = "";
+    settings.data.url = "https://en.wikipedia.org/wiki/Special:Random";
+    settings.data.sentences = $("#sentences-input").val().trim();
+    
+    $.ajax(settings).done(function (response) {
+        $("#text-output").html(response.summary);
+    });
+});
+
+// Speech button on click event
+$("#speechButton").on("click", function (event) {
+    event.preventDefault();
 
     var params = $.param({
         key: 'a64321115a4341239e25f9def1867dac',
         src: $("#text-output").text(),
         // hl: 'en-us',
         hl: $("#language").val(),
-        r: 0, 
+        r: 0,
         c: 'mp3',
         f: '44khz_16bit_stereo',
         ssml: false
     });
 
     var url = "http://api.voicerss.org/?" + params;
-    
+
     $("#audioPlayer").attr("src", url);
 });
 
-$("#stopButton").on("click", function(event) {
+// Stop button on click event
+$("#stopButton").on("click", function (event) {
     event.preventDefault();
     $("audio").attr("src", "");
 });
